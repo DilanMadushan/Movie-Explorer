8908d73b72d4893e700ddea9486dbe62a7c32d43
Dilan Fernando <dilanmadushanka6013@gmail.com>
2025-05-10 12:13:04 +0530

    responsive the nav bar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,5 +1,4 @@
-import React from "react";
-import { useState } from "react";
+import React, { useState } from "react";
 import {
   AppBar,
   Box,
@@ -16,9 +15,9 @@ import { styled } from "@mui/system";
 import DeblurIcon from "@mui/icons-material/Deblur";
 import SearchIcon from "@mui/icons-material/Search";
 import MenuIcon from "@mui/icons-material/Menu";
-import CloseIcon from '@mui/icons-material/Close';
-import { useDispatch, useSelector } from "react-redux";
-import { findMoviebyName, getAllMovies } from "../slice/MovieSlice";
+import CloseIcon from "@mui/icons-material/Close";
+import { useDispatch } from "react-redux";
+import { findMoviebyName } from "../slice/MovieSlice";
 import { Link } from "react-router-dom";
 
 const StyledToolBar = styled(Toolbar)({
@@ -26,62 +25,72 @@ const StyledToolBar = styled(Toolbar)({
   justifyContent: "space-between",
 });
 
-const SearchBar = styled(Box)({
-  backgroundColor: "white",
-  padding: "0 10px",
-  borderRadius: "1rem",
-  width: "40%",
-  display: "flex",
-  alignItems: "center",
-  gap: "10px",
-});
-
-const SearchBarModel = styled(Box)({
+const SearchBarModel = styled(Box)(({ theme }) => ({
   backgroundColor: "#151414",
   padding: "0 10px",
   borderRadius: "8px",
-  width: "50%",
   display: "flex",
   alignItems: "center",
   gap: "10px",
   boxShadow: "0 0 10px rgba(194, 194, 194, 0.1)",
+  width: "50%",
+  [theme.breakpoints.down("md")]: {
+    display: "none",
+  },
+}));
+
+const SearchBarMobile = styled(Box)({
+  backgroundColor: "#151414",
+  padding: "10px",
+  borderRadius: "8px",
+  display: "flex",
+  alignItems: "center",
+  gap: "10px",
+  marginBottom: "20px",
+  border: "1px solid #333",
 });
 
 const Content = styled(Box)(({ theme }) => ({
   display: "flex",
   alignItems: "center",
   gap: "20px",
-  [theme.breakpoints.down("sm")]: {
+  [theme.breakpoints.down("md")]: {
     display: "none",
+  },
+  
+  "& a": {
+    color: "white",
+    fontWeight: 500,
+    textDecoration: "none",
+    "&:hover": {
+      color: "red",
+    },
   },
 }));
 
-
 const NavBar = () => {
-
-const dispatch = useDispatch();
-
-const movies = useSelector((state) => state.movie);
-
-const findMovie = (movie) => {
-    dispatch(findMoviebyName(movie));
-};
+  const dispatch = useDispatch();
   const [open, setOpen] = useState(false);
 
+  const findMovie = (movie) => {
+    dispatch(findMoviebyName(movie));
+  };
+
   const contentList = [
-  { text: "Home", path: "/home" },
-  { text: "Trending", path: "/trending" },
-  { text: "Favorites", path: "/favourite" },
-];
+    { text: "Home", path: "/home" },
+    { text: "Trending", path: "/trending" },
+    { text: "Favorites", path: "/favourite" },
+  ];
 
   return (
-    <AppBar position="sticky" sx={{ bgcolor: "#151414", overflowX: "hidden"}}>
+    <AppBar position="sticky" sx={{ bgcolor: "#151414", px: 2 }}>
       <StyledToolBar>
         <Box sx={{ display: "flex", alignItems: "center", gap: "10px" }}>
-          <DeblurIcon sx={{color: "red",width: "50px",height: "50px"}}/>
+          <DeblurIcon sx={{ color: "red", fontSize: { xs: 30, sm: 40, md: 50 } }} />
           <Typography
-            variant="h5"
-            fontFamily={"serif"}
+            variant="h6"
+            color="white"
+            sx={{ fontSize: { xs: "1.2rem", sm: "1.5rem", md: "1.8rem" } }}
           >
             Movie Explorer
           </Typography>
@@ -96,6 +105,7 @@ const findMovie = (movie) => {
             sx={{
               height: "45px",
               color: "white",
+              fontSize: "1rem",
               border: "none",
             }}
           />
@@ -103,18 +113,14 @@ const findMovie = (movie) => {
 
         <Content>
           {contentList.map((item) => (
-            <Link
-              to={item.path}
-              style={{ textDecoration: "none", color: "white" }}
-              key={item.text}
-            >
+            <Link key={item.text} to={item.path}>
               {item.text}
             </Link>
           ))}
         </Content>
 
         <IconButton
-          sx={{ display: { xs: "block", sm: "none" }, color: "white" }}
+          sx={{ display: { xs: "block", md: "none" }, color: "white" }}
           onClick={() => setOpen(true)}
         >
           <MenuIcon />
@@ -122,11 +128,60 @@ const findMovie = (movie) => {
       </StyledToolBar>
 
       <Drawer anchor="right" open={open} onClose={() => setOpen(false)}>
-        <Box sx={{ width: "60vw", height: "100vh", bgcolor: "#151414",fontSize: "16px" }}>
+        <Box
+          sx={{
+            width: "65vw",
+            height: "100vh",
+            bgcolor: "#151414",
+            color: "white",
+            py: 4,
+            px: 2,
+          }}
+        >
+
+          <Box sx={{ display: "flex", justifyContent: "flex-end", mb: 2 }}>
+            <IconButton onClick={() => setOpen(false)} sx={{ color: "white" }}>
+              <CloseIcon />
+            </IconButton>
+          </Box>
+
+
+          <SearchBarMobile>
+            <SearchIcon sx={{ color: "grey" }} />
+            <InputBase
+              placeholder="Search movies..."
+              fullWidth
+              onChange={(e) => findMovie(e.target.value)}
+              sx={{
+                height: "40px",
+                color: "white",
+                fontSize: "1rem",
+                border: "none",
+              }}
+            />
+          </SearchBarMobile>
+
+
           <List>
-            {contentList.map((text) => (
-              <ListItem button key={text} sx={{ color: "white",marginTop: "20px" }}>
-                <ListItemText primary={text} fontFamily={""}/>
+            {contentList.map((item) => (
+              <ListItem
+                button
+                key={item.text}
+                component={Link}
+                to={item.path}
+                onClick={() => setOpen(false)}
+                sx={{
+                  color: "white",
+                  borderBottom: "1px solid #2e2e2e",
+                  py: 2,
+                  "&:hover": {
+                    color: "red",
+                  },
+                }}
+              >
+                <ListItemText
+                  primary={item.text}
+                />
               </ListItem>
             ))}
           </List>
